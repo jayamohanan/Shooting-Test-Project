f88dad66627c3fec4817c7f36b6fecedf53e7502
d <d@DESKTOP-85DL300>
2020-01-14 04:05:46 +0530

    commit

--- Assets/delete/delete.cs
+++ Assets/delete/delete.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class delete : MonoBehaviour
 {
+    public AudioClip audioClip;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = audioClip;
     }
 
     // Update is called once per frame
@@ -16,6 +19,7 @@ public class delete : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-0.5f,0.5f),Random.Range(-0.5f,0.5f),1)*10000);
+            audioSource.Play();
         }
     }
 }
